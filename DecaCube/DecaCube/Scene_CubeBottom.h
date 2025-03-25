72a96fe4d0f36c5fd204e2bd7fd65ce98c396db1
Nick DeLuca <nickid24@mail.com>
2025-03-25 09:20:49 -0300

    added enemy behaviour to bottom side of cube

--- DecaCube/DecaCube/Scene_CubeBottom.h
+++ DecaCube/DecaCube/Scene_CubeBottom.h
@@ -39,6 +39,16 @@ class Scene_CubeBottom : public Scene
     void sAnimation();
     void sCollision();
     void sEnemyFaceChange(sf::Time dt);
+    void sEnemyBehaviour();
+
+
+    void flipper(std::shared_ptr<Entity> entity);
+
+    std::vector<Vec2> getAvailableNodes(Vec2 pos, std::shared_ptr<Entity> entity);
+
+    Vec2 pickBestNode(std::vector<Vec2> availableNodes);
+
+    void enemyAwareMovement(std::shared_ptr<Entity> enemy);
 
     // helper functions
     void	                onEnd() override;
@@ -47,12 +57,13 @@ class Scene_CubeBottom : public Scene
     void spawnPlayer(sf::Vector2f pos);
     void loadLevel(const std::string& path);
     Vec2 gridToMidPixel(float gridX, float gridY, std::shared_ptr<Entity> entity);
+    Vec2 midPixelToGrid(float midX, float midY, std::shared_ptr<Entity> entity);
     void loadFromFile(const std::string& path);
     void playerMovement();
     void adjustPlayerPosition();
     void snapToGrid(std::shared_ptr<Entity> entity);
-    bool canMoveInDirection(std::string direction);
-    sPtrEntt getCurrentTile();
+    bool canMoveInDirection(std::string direction, std::shared_ptr<Entity> entity);
+    sPtrEntt getCurrentTile(std::shared_ptr<Entity> entity);
     void checkIfPlayerInBounds();
     void fixPlayerPos();
     std::string getRotatedTileName(std::string name, int rotations);
@@ -60,6 +71,8 @@ class Scene_CubeBottom : public Scene
     Vec2 rotateEntityPosition(Vec2 prePos);
     void rotateEntireFace();
     int changeFace(int currentFace, bool isFlipper);
+
+    bool alreadyTraveled(std::vector<Vec2> visitedNodes, Vec2 targetNode);
 
 public:
     Scene_CubeBottom(GameEngine* gameEngine, const std::string& levelPath);
