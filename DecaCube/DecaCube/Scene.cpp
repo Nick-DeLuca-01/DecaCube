46f0fb353c2dcbd04b23bb9cdd685499c11d597b
Nick DeLuca <nickid24@mail.com>
2025-04-09 17:39:01 -0300

    added methods in Scene for changing the song

--- DecaCube/DecaCube/Scene.cpp
+++ DecaCube/DecaCube/Scene.cpp
@@ -1,4 +1,5 @@
 #include "Scene.h"
+#include "MusicPlayer.h"
 
 
 Scene::Scene(GameEngine* gameEngine) : _game(gameEngine)
@@ -33,4 +34,40 @@ const CommandMap Scene::getActionMap() const
 void Scene::registerAction(int inputKey, std::string command)
 {
 	_commands[inputKey] = command;
+}
+
+void Scene::changeMusic()
+{
+	int collectedItems = _playerData.collectedItems.size();
+
+	switch (collectedItems) {
+	case 2:
+	case 3:
+		changeMusic("danger1");
+		break;
+	case 4:
+	case 5:
+		changeMusic("danger2");
+		break;
+	case 6:
+	case 7:
+		changeMusic("danger3");
+		break;
+	case 8:
+	case 9:
+		changeMusic("danger4");
+		break;
+	case 10:
+		changeMusic("danger5");
+		break;
+	}
+}
+
+void Scene::changeMusic(std::string songName)
+{
+	sf::Time playingOffset = MusicPlayer::getInstance().getPlayingOffset();
+	MusicPlayer::getInstance().stop();
+	MusicPlayer::getInstance().play(songName);
+	MusicPlayer::getInstance().setPlayingOffset(playingOffset);
+	MusicPlayer::getInstance().setVolume(80);
 }
