1a7d64199da1f20e70537f1c450238af27c6d58d
Nick DeLuca <nickid24@mail.com>
2025-04-09 17:10:14 -0300

    added music and sounds to files and sound paths to config

--- DecaCube/DecaCube/MusicPlayer.cpp
+++ DecaCube/DecaCube/MusicPlayer.cpp
@@ -7,8 +7,15 @@
 
 
 MusicPlayer::MusicPlayer() {
-    m_filenames["menuTheme"] = "../assets/Music/MenuTheme.ogg";
-    m_filenames["gameTheme"] = "../assets/Music/MissionTheme.ogg";
+    m_filenames["menuTheme"] = "../assets/music/menu.wav";
+    m_filenames["winTheme"] = "../assets/music/win.wav";
+    m_filenames["loseTheme"] = "../assets/music/lose.wav";
+    m_filenames["danger0"] = "../assets/music/decacube_danger0.wav";
+    m_filenames["danger1"] = "../assets/music/decacube_danger1.wav";
+    m_filenames["danger2"] = "../assets/music/decacube_danger2.wav";
+    m_filenames["danger3"] = "../assets/music/decacube_danger3.wav";
+    m_filenames["danger4"] = "../assets/music/decacube_danger4.wav";
+    m_filenames["danger5"] = "../assets/music/decacube_danger5.wav";
 }
 
 void MusicPlayer::addSong(const std::string& name, const std::string& path) {
